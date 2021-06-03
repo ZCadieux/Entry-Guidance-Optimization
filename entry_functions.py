f4b55f7451fb8e5501c29111b4b5ecaab1bf1367
ZCadieux <zcadieux@ucsd.edu>
2021-06-03 14:27:53 -0700

    Function bug fixes

--- entry_functions.py
+++ entry_functions.py
@@ -15,6 +15,10 @@ class EntryFunctions(ot.Group):
         #flatten = self.options['flatten']
         area = 159.94 #from vehicle data
 
+        #instead of as parameters to setup function, should this be declared here?
+        #r = self.declare_input('r', shape=(num,1)) #radial distance from center of Mars
+        #V = self.declare_input('V', shape=(num,1)) #Mars relative velocity
+
         #input r, V; output vector with density, Vsound, lift, drag
         #Step 1: Convert to geodetic, to get altitude (r and height in km)
         #in beginning, using just geocentric
@@ -29,7 +33,7 @@ class EntryFunctions(ot.Group):
         E = 7.041319756467658*10^-8; F = -0.04399163896948651
         G = 0.0006555968430113561; H = -3.709114148189494*10^-6
         I = 8.137552734673534*10^-9
-        vars[0] = ot.exp((A+r(B+r(C+r(D+r(E)))))/(1.0+r(F+r(G+r(H+r(I)))))) #density
+        vars[0] = ot.exp((A+r*(B+r*(C+r*(D+r*E))))/(1.0+r*(F+r*(G+r*(H+r*I))))) #density
 
         #Step 3, use fit polynomial for Vsound
         A = 3.90812820*10^2; B = -5.07169986*10^3
@@ -38,13 +42,13 @@ class EntryFunctions(ot.Group):
         G = 2.74880412*10^6; H = -1.60759656*10^6
         I = 3.84982694*10^5
         if r < 10000:
-            x  = 10000
+            x  = 10000.
         elif r > 130000:
-            x = 130000
+            x = 130000.
         else:
             x = r
         x = x*1000/130081.410992
-        vars[1] = A + x(B + x(C + x(D + x(E + x(F + x(G + x(H + I*x))))))) #speed of sound
+        vars[1] = A + x*(B + x*(C + x*(D + x*(E + x*(F + x*(G + x*(H + I*x))))))) #speed of sound
 
         #Step 4, find CL, CD
         x = V/vars[1] #find mach number
@@ -58,11 +62,15 @@ class EntryFunctions(ot.Group):
 
         x = 1/x, A = 1.015355733524931; B = -0.04826676756876537
         C = 1.381399436898735; D = -0.9962918706041476
-        C_D = A + x(B + x(C + x(D)))
+        C_D = A + x*(B + x*(C + x*D))
 
         coeff = vars[1] * V^2 * area/2
 
         vars[2] = C_L * coeff #lift
         vars[3] = C_D * coeff #drag
+
+        #self.register_output('lift', vars[2])
+        #self.register_output('drag', vars[3])
+        #instead of as a return, should this be outputted here?
 
         return vars
